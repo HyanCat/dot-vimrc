ada7bc977734cfbf2250e8202007f710bc3fe253
HyanCat <hyancat@live.cn>
2015-05-12 19:23:27 +0800

    update readme.

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ mv ~/.vim ~/.vim.old.$date
 mv ~/.vimrc ~/.vimrc.old.$date
 
 git clone git://github.com/HyanCat/dot-vimrc.git ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 vim +BundleInstall +qall
+
+ln -s ~/.vim/vimrc ~/.vimrc
